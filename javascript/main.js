a70a3a852fe0c4d67b3a0b10ac45f9d0b92b72f9
alternio <38168651+alternio@users.noreply.github.com>
2018-04-08 00:59:21 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,10 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+// Create the map
+var map = L.map('map').setView([39.5, -0.5], 5);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+// Set up the OSM layer
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    {maxZoom: 18}).addTo(map);
+
+// add a marker in the given location
+L.marker([36.83711,-2.464459]).addTo(map);

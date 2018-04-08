ff3e7d12eac726335f239f322d2fe5d96d1b2f72
alternio <38168651+alternio@users.noreply.github.com>
2018-04-08 01:22:55 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -8,3 +8,10 @@ L.tileLayer(
 
 // add a marker in the given location
 L.marker([36.83711,-2.464459]).addTo(map);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);

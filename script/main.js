6916867ebcd8fd22f2e0b41e918b2149bd10605f
alternio <38168651+alternio@users.noreply.github.com>
2018-04-08 02:32:03 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -13,21 +13,3 @@ var marker = L.marker([36.83711,-2.464459]);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 marker.addTo(map);
-
-
-
-
-
-map.createPane('labels');
-map.getPane('labels').style.zIndex = 650;
-map.getPane('labels').style.pointerEvents = 'none';
-var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB'
-}).addTo(map);
-
-var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB',
-        pane: 'labels'
-}).addTo(map);
-
-var geojson = L.geoJson(GeoJsonData, geoJsonOptions).addTo(map);

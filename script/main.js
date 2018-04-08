804c412628c72bc70412978d0142a5b159c2b993
alternio <38168651+alternio@users.noreply.github.com>
2018-04-08 01:53:35 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,9 +2,9 @@
 var map = L.map('map').setView([39.5, -0.5], 5);
 
 // Set up the OSM layer
-L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
 }).addTo( map );
 
 // add a marker in the given location

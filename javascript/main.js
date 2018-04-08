a6682ca9b6221dfd2ae5762cd91ae243a4d24c24
alternio <38168651+alternio@users.noreply.github.com>
2018-04-08 01:06:40 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ var map = L.map('map').setView([39.5, -0.5], 5);
 
 // Set up the OSM layer
 L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
     {maxZoom: 18}).addTo(map);
 
 // add a marker in the given location
